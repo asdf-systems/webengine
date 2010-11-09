a5bd6f57ee2b3f9ab7a8b2d129a9cd53d2f8bdb2
Alexander Surma <surma@78762.de>
2010-11-09 16:26:34 +0100

    Preview has adjustable size and color

--- src/admin/default_page.php
+++ src/admin/default_page.php
@@ -2,7 +2,9 @@
 	function refreshImage() {
 		var image = document.getElementById("renderimage");
 		var text = escape(document.getElementById("renderinput").value);
-		image.src="textrenderer/textrenderer.php?text="+text;
+		var col = escape(document.getElementById("fcolor").value);
+		var size = escape(document.getElementById("fsize").value);
+		image.src="textrenderer/textrenderer.php?color="+col+"&size="+size+"&text="+text;
 	}
 </script>
 <center>
@@ -14,11 +16,17 @@
 		</tr>
 		<tr>
 			<td align="center">
+				Size: <input type="text" id="fsize" value="10"> Color: <input type="text" id="fcolor" value="#000000">
+			</td>
+		</tr>
+		<tr>
+			<td align="center">
 				<img id="renderimage" src="textrenderer/textrenderer.php" style="border: 1px solid green">
 			</td>
 		</tr>
 		<tr>
 			<td>
+				<input type="button" value="Refresh image" onClick="refreshImage()">
 				<form action="<?=$PHP_SELF;?>" method="post">
 					<input type="hidden" name="action" value="compile">
 					<input type="submit" value="Refresh website">
