733e2a3a47b2d3429e3287df0952fb79588d2ea1
Alexander Surma <surma@78762.de>
2011-02-06 20:31:45 +0100

    Modified globals

--- testCases/testCaseEngine/Beta/EngineGlobals.js
+++ testCases/testCaseEngine/Beta/EngineGlobals.js
@@ -2,8 +2,8 @@ var gCurrentAnimationSpeed;
 var gCurrentCallbackFunction;
 var globals = {
     debug : 1,
-    start : "main/news/",
-    static : "main/static/,main/navi/",
+    start : "main/news/,ecken/gelb/",
+    static : "main/,main/static/,main/navi/",
     stdUnit : "px", 
     standardFontSize : "12pt",
     standardFontFamily : "Arial, Tahoma",
