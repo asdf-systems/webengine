03f182de5867c804df41efbb69f6bc89985a186a
Alexander Surma <surma@78762.de>
2011-02-06 22:31:05 +0100

    Fixed corners

--- testCases/testCaseEngine/Beta/EngineGlobals.js
+++ testCases/testCaseEngine/Beta/EngineGlobals.js
@@ -2,9 +2,9 @@ var gCurrentAnimationSpeed;
 var gCurrentCallbackFunction;
 var globals = {
     debug : 1,
-    start : "main/news/,ecken/gelb/",
-    static : "main/,main/static/,main/navi/",
-    stdUnit : "px", 
+    start : "main/news/",
+    static : "main/,main/static/,main/navi/,corners/",
+    stdUnit : "px",
     standardFontSize : "12pt",
     standardFontFamily : "Arial, Tahoma",
     standardFontColor : "#ababa4",
