297417242574ea2dbc4c476d8a233049c1ce3c8f
Alexander Surma <surma@78762.de>
2011-02-10 21:44:23 +0100

    Content is now centered

--- testCases/testCaseEngine/Beta/EngineGlobals.js
+++ testCases/testCaseEngine/Beta/EngineGlobals.js
@@ -2,8 +2,8 @@ var gCurrentAnimationSpeed;
 var gCurrentCallbackFunction;
 var globals = {
     debug : 1,
-    start : "main/news/",
-    static : "main/,main/static/,main/navi/,corners/",
+    start : "main/wrap/news/",
+    static : "main/,main/wrap/,main/wrap/static/,main/wrap/navi/,corners/",
     stdUnit : "px",
     standardFontSize : "12pt",
     standardFontFamily : "Arial, Tahoma",
